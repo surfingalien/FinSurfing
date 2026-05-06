38db0d92693bf575a42ec739f28a6523a5c71239
SurfingAlien <suhasgm@gmail.com>
2026-05-06 13:11:54 -0500

    Fix: use CommonJS for serverless fn, add package.json for Node runtime

--- Downloads/FinSurfing/api/proxy.js
+++ Downloads/FinSurfing/api/proxy.js
@@ -55,7 +55,7 @@ async function getSession() {
   return refreshSession();
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
